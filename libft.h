a0d69dec478c9c10a0eadf64c83f072b6dc560b6
leldiss <urankaarov@gmail.com>
2021-10-15 17:44:04 +0300

    first

--- libft.h
+++ libft.h
@@ -30,6 +30,7 @@ int		ft_isascii(int c);
 int		ft_atoi(char *str);
 int		ft_isdigit(int c);
 int		ft_isprint(int c);
+void	*ft_calloc(size_t num, size_t size);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
