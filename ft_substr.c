c64fbaab69423b8712f07ce2f4930fb9590b3a62
leldiss <urankaarov@gmail.com>
2021-10-15 15:43:52 +0300

    third

--- ft_substr.c
+++ ft_substr.c
@@ -18,10 +18,12 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	char			*str;
 
 	i = 0;
+	if (!s)
+		return (NULL);
 	str = (char *)malloc(sizeof(char) * len + 1);
 	if (str == NULL)
 		return (NULL);
-	while (i < len)
+	while (i < len && s[start])
 	{
 		str[i] = s[start + i];
 		i++;
